67a8e8cdff750df0a9aea41c631a630ef45e2250
rupert-ong <rupert.ong@gmail.com>
2021-03-20 15:32:34 -0400

    Setup webpack compilation for dev files

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from "express";
 import open from "open";
 import path from "path";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));

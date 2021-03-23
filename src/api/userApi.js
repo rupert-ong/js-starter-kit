3a65eecf79c35f233851fc7e4b4960c493d924bb
rupert-ong <rupert.ong@gmail.com>
2021-03-22 21:16:43 -0400

    Add delete utility

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,9 +7,20 @@ export function getUsers() {
   return get("users");
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 // Centralized API logic
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: "DELETE",
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import numeral from "numeral";
-import { getUsers } from "./api/userApi";
+import { deleteUser, getUsers } from "./api/userApi";
 import "./index.css";
 
 const courseValue = numeral(1000).format("$0,0.00");
@@ -22,4 +22,16 @@ getUsers().then((result) => {
   });
 
   global.document.getElementById("users").innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName("deleteUser");
+
+  Array.from(deleteLinks, (link) => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
